5bdb871a7dce559689829e635222216855d25d80
tpereira <tpereira@student.42lisboa.com>
2023-09-16 08:51:00 +0100

    more tests for ex00

--- ex00/includes/Easyfind.tpp
+++ ex00/includes/Easyfind.tpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 18:57:41 by tpereira          #+#    #+#             */
-/*   Updated: 2023/09/11 19:45:05 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/09/16 08:49:53 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@
 template <typename T>
 void easyfind(T& a, int b)
 {
+	if (a.empty())
+		std::cout << "Container is empty!" << std::endl;
 	// if nothing found, std::find() returns a.end()
-	if (std::find(a.begin(), a.end(), b) != a.end())
-		std::cout << "Found " << b << " in provided array!" << std::endl;
+	else if (std::find(a.begin(), a.end(), b) != a.end())
+		std::cout << "Found " << b << " in provided container!" << std::endl;
 	else
 		std::cout << "Sorry, " << b << " not found!" << std::endl;
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,20 +6,59 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 13:20:18 by tpereira          #+#    #+#             */
-/*   Updated: 2023/09/11 19:43:06 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/09/16 08:49:38 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <array>
+#include <vector>
+#include <list>
+// #include <map>
+
 #include "Easyfind.hpp"
 
 int main()
 {
-	std::array<int, 3> arr{1, 2, 3};
+	std::array<int, 3> arr = {1, 2, 3};
+	std::vector<int> vec;
+	std::list<int> lst;
+	std::list<int> empty;
+	// don't need to use Map as it's an associative container (key-value)
+	// std::map<int, int> map;
+	
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
 
+	lst.push_back(1);
+	lst.push_back(2);
+	lst.push_back(3);
+
+	// map[1] = 1;
+	// map[2] = 2;
+	// map[3] = 3;
+
+	// std::cout << map.begin()->first << std::endl;
+
+	std::cout << "Array " << std::endl;
 	easyfind(arr, 2);
 	easyfind(arr, 5);
+
+	std::cout << "\nVector " << std::endl;
+	easyfind(vec, 2);
+	easyfind(vec, 5);
+
+	std::cout << "\nList " << std::endl;
+	easyfind(lst, 2);
+	easyfind(lst, 5);
+
+	std::cout << "\nList (empty)" << std::endl;
+	easyfind(empty, 2);
+	easyfind(empty, 5);
+
+	// easyfind(map, 2);
+	// easyfind(map, 5);
 	
    return 0;
 }
